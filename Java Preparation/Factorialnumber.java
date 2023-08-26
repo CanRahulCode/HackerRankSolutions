6794a421e899c5f0653bcba234485b0fc5b70942
CanRahulCode <avulapatirahul@gmail.com>
2023-08-26 22:22:57 +0530

    Java program

---
+++ Java Preparation/Factorialnumber.java
@@ -0,0 +1,19 @@
+package practice;
+import java.util.Scanner;
+public class Factorialnumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=1;
+		int b=1;
+		for(int i=1;i<=n;i++) {
+			System.out.print("The Factorial of:"+a);
+			int sum=a+b;
+			a=b;
+			b=sum;
+			
+		}
+	}
+
+}
